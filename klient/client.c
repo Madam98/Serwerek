76c58237d6b22847e0dbdfa5bd873bd33c20c739
Madam98 <adambluebox1@gmail.com>
2022-01-30 16:30:07 +0100

    epoll change

--- klient/client.c
+++ klient/client.c
@@ -1,275 +1,187 @@
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/epoll.h>
+#include <Headers/headers/headers.h>
+#include <Headers/data/variables.h>
+#include <Headers/data/structs.h>
 
-#define MAXBUF 1024
-#define NUM_THREADS 5
-#define MAX_EPOLL_EVENTS 64
+#include <Headers/file_opearation/readstring.h>
 
-//STWORZENIE WATKU WYSYLAJACY ZAPYTANIA KONTROLNE
-//co 20 sekund
+#include <Headers/file_opearation/msleep.h>
+#include <Headers/file_opearation/textAnimation.h>
 
-//struktura zawierajaca dane, ktołre zostana przekazane do watku
-struct thread_data_t
+#include <Headers/connect_operation/createconnectionfunctions.h>
+
+#include <Headers/arrayfunctions.h>
+#include <Headers/thread/threadfunctions.h>
+#include <Headers/connect_operation/handleconnection.h>
+#include <Headers/connect_operation/handlearrayofclients.h>
+
+#include <Headers/handler/sigsegv.h>
+
+#include <Headers/file_opearation/createfolder.h>
+
+#include <Headers/commands/touch.h>
+#include <Headers/commands/share.h>
+#include <Headers/commands/list.h>
+#include <Headers/commands/delete.h>
+#include <Headers/commands/copy.h>
+#include <Headers/commands/rename.h>
+
+//-----------------------------------------------------------------------------
+/*
+TO DO user commands
+1. Stworzyć plik
+2. Udostępnić plik
+3. Lista połączonych użytkowników
+4. Usunąć plik
+5. Skopiować plik
+6. Zmienić nazwę pliku
+ */
+//-----------------------------------------------------------------------------
+
+int main(int argc, char* argv[])
 {
-    int server_descriptor;
-    char read;
-};
+    struct thread_data_t *t_data_ptr = malloc(sizeof(struct thread_data_t));
+    struct array_data_clients *array = malloc(sizeof(struct array_data_clients));
+    struct epoll_event event, events[MAX_EVENTS];
+    struct clients_tab_map map_clients[100];
 
-//wskaznik na funkcjr opisujaca zachowanie watku
-void *ThreadBehavior(void *t_data)
-{
-    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
-    //dostep do całej struktury: (*th_data).pole
+    //PRZYGOTOWYWANIE STRUKTUR
+    createThreadClient(max_num_of_clients, t_data_ptr);
+    createArrayClient(array);
+
+    initMutex(t_data_ptr);
+    initAdreessServer(&server_address);
 
 
-    char buf0[12]  = "uzytkownik3";
-    char buf1[30]  = "touch dam lubi orzeszki";
-    char buf2[30]  = "delete borowki";
-    char buf3[40]  = "rename dam lubi orzeszki > Adam";
-    char buf4[5]   = "q";
+    //UTWORZENIE GNIAZDA DO POLACZENIA Z SERWEREM
+    server_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    checkServerSocketDescriptor(server_socket_descriptor, argv[0]);
+    setsockopt(server_socket_descriptor, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse_addr_val, sizeof(reuse_addr_val));
+    socketInfo(server_socket_descriptor);
+    //fcntl(server_socket_descriptor, F_SETFL, O_NONBLOCK);
 
-    char want[4] = "want";
-    char answer[20] = "";
 
-    printf("Wysylam nazwe uzytkownika: %s\n", buf0);
-    send((*th_data).server_descriptor, buf0, sizeof(buf0), MSG_CONFIRM);
+    //DOWIAZANIE ADRESU SERWERA DO STWORZONEGO GNIAZDA
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = handler;
+    sigaction(SIGSEGV, &action, NULL);
 
-    while (strlen(answer) == 0) {
-        printf("WCHODZE \n");
-        send((*th_data).server_descriptor, want, sizeof(want), MSG_CONFIRM);
 
-        recv((*th_data).server_descriptor, answer, sizeof(answer), MSG_DONTWAIT);
-        printf("%s\n", answer);
+    //POWIAZANIE ADRESU Z GNIAZDEM
+    bind_result = bind(server_socket_descriptor, (struct sockaddr *) &server_address, sizeof(struct sockaddr));
+    while(bind_result == -1) {
+        printf("Powyzszy port jest zajety! Czekam na zwolnienie portu...\n");
+        textAnimation();
+        bind_result = bind(server_socket_descriptor, (struct sockaddr *) &server_address, sizeof(struct sockaddr));
+    }
+    checkBindResult(bind_result, argv[0]);
+    bindInfo();
 
+
+    //WLACZENIE NASLUCHIWANIA NA UTWORZONYM GNIEZDZIE
+    listen_result = listen(server_socket_descriptor, QUEUE_SIZE);
+    checkListen(listen_result, argv[0]);
+    infoListen();
+
+
+    //STWORZENIE FOLDERU Z POTENCJLANYMI DOKUMENTAMI(jesli nie istnieja)
+    char *path = getenv("PWD");
+    char* info = "W folderu z wykonywanym plikiem musi znajdowac sie folder DOCUMENTS\n";
+    folderInfo(path, info);
+
+
+    //STWORZENIE EPOLL
+    //https://www.programmerall.com/article/1404354560/
+    memset(&events, 0, sizeof(struct epoll_event));
+    int epoll_fd = createEpoll();
+    //event.events = EPOLLIN|EPOLLET;
+    event.events = EPOLLIN;
+    event.data.fd = server_socket_descriptor;
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_socket_descriptor, &event) == -1){
+        fprintf(stderr, "Blad epoll_ctl ADD dla serwer socket\n");
+        close(epoll_fd);
+        return 1;
+    }
+    event.data.fd = 0;
+    if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event) == -1){
+        fprintf(stderr, "Blad epoll_ctl ADD dla klawiatury\n");
+        close(epoll_fd);
+        return 1;
     }
 
-
-    printf("Wysylam komende uzytkownika: %s\n", buf1);
-    send((*th_data).server_descriptor, buf1, sizeof(buf1), MSG_CONFIRM);
-
-    memset(answer, 0, 6);
-    printf("%s\n", answer);
-    while (strlen(answer) == 0) {
-        printf("WCHODZE \n");
-        send((*th_data).server_descriptor, want, sizeof(want), 0);
-
-        recv((*th_data).server_descriptor, answer, sizeof(answer), MSG_DONTWAIT);
-        printf("%s\n", answer);
-
-    }
-
-    printf("Wysylam komende uzytkownika: %s\n", buf2);
-
-    send((*th_data).server_descriptor, buf2, sizeof(buf2), MSG_CONFIRM);
-
-    memset(answer, 0, 6);
-    while (strlen(answer) == 0) {
-        printf("WCHODZE \n");
-        send((*th_data).server_descriptor, want, sizeof(want), MSG_CONFIRM);
-
-        recv((*th_data).server_descriptor, answer, sizeof(answer), MSG_DONTWAIT);
-        printf("%s\n", answer);
-
-    }
-
-    printf("Wysylam komende uzytkownika: %s\n", buf3);
-    sleep(1);
-    write((*th_data).server_descriptor, buf3, 40);
-
-    memset(answer, 0, 6);
-    while (strlen(answer) == 0) {
-        printf("WCHODZE \n");
-        send((*th_data).server_descriptor, want, sizeof(want), MSG_CONFIRM);
-        sleep(2);
-        recv((*th_data).server_descriptor, answer, sizeof(answer), MSG_DONTWAIT);
-        printf("%s\n", answer);
-        sleep(2);
-    }
-
-    printf("Wysylam komende uzytkownika: %s\n", buf4);
-    write((*th_data).server_descriptor, buf4, 5);
-
-
-    //send((*th_data).server_descriptor, (const void*)(*th_data).read, strlen((*th_data).read), 0);
-
-
-
-    pthread_exit(NULL);
-}
-
-
-//funkcja obslugujacaca polaczenie z serwerem
-void handleConnection(int connection_socket_descriptor) {
-    //wynik funkcji tworzacej watek
-    int create_result = 0;
-
-    //uchwyt na watek
-    pthread_t thread1;
-
+    //OBSLUGA POLACZEN Z UZYTKOWNIKAMI
+    int epoll_des;
     int i;
-    int num_ready;
+    int counter = 0;
+    struct user_data strUser;
+    char *temp;
 
-    /*---Wait for socket connect to complete---*/
-
-    //num_ready = epoll_wait(epfd, events, MAX_EPOLL_EVENTS, 10000/*timeout*/);
-
-    /*for(i = 0; i < num_ready; i++) {
-        if(events[i].events & EPOLLIN) {
-            printf("Socket %d connected\n", events[i].data.fd);
-        }
-    }
-    char buffer[MAXBUF];
-    */
-
-    //dane, ktore zostana przekazane do watku
-    struct thread_data_t t_data;
-
-    t_data.server_descriptor = connection_socket_descriptor;
-    //t_data.read = "touch";
-
-    //printf("%s\n", t_data.read);
-
-    create_result = pthread_create(&thread1, NULL, ThreadBehavior, (void *)&t_data);
-    printf("%d\n", create_result);
-    if (create_result){
-        printf("Blad przy probie utworzenia watku, kod bledu: %d\n", create_result);
-        exit(-1);
-    }
-    //printf("Czekam jak odbiore\n");
-    //char buf[100];
-    //read(connection_socket_descriptor, buf, 12);
-
-    //printf("Odebralem\n");
-}
-
-void *ThreadRequest(void *t_data){
-    printf("Sprawdzam odpowiedz\n");
-
-    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
-    //dostep do całej struktury: (*th_data).pole
-
-    write((*th_data).server_descriptor, "", 0);
-
-    //zamkniecie watku
-
-    pthread_exit(NULL);
-
-}
-
-void handleRequest(int connection_socket_descriptor){
-    //wynik funkcji tworzacej watek
-    int create_result = 0;
-    //uchwyt na watek
-    pthread_t thread1;
-    //dane, ktore zostana przekazane do watku
-    struct thread_data_t t_data;
-
-    create_result = pthread_create(&thread1, NULL, ThreadRequest, (void *)&t_data);
-    //printf("%d\n", create_result);
-    if (create_result){
-        printf("Blad przy probie utworzenia watku, kod bledu: %d\n", create_result);
-        exit(-1);
-    }
-}
-
-int main (int argc, char *argv[])
-{
-    int connection_socket_descriptor;
-    int connect_result;
-    struct sockaddr_in server_address;
-    struct hostent* server_host_entity;
-    struct epoll_event events[MAX_EPOLL_EVENTS];
-
-    argv[1] = "127.0.0.1";
-    argv[2] = "1235";
-    argv[3] = "Adam_Example"; //<----user
-
-    // if (argc != 3){
-    //     fprintf(stderr, "Zly sposob uzycia: %s server_name port_number\n", argv[0]);
-    //     exit(1);
-    // }
-
-    server_host_entity = gethostbyname(argv[1]);
-    if (! server_host_entity){
-        fprintf(stderr, "%s: Nie mozna uzyskac adresu IP serwera.\n", argv[0]);
-        exit(1);
-    }
-
-    connection_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
-    if (connection_socket_descriptor < 0){
-        fprintf(stderr, "%s: Blad przy probie utworzenia gniazda.\n", argv[0]);
-        exit(1);
-    }
-
-    //int epfd = epoll_create(1);
-    //struct epoll_event event;
-    //event.events = EPOLLIN; // Cann append "|EPOLLOUT" for write events as well
-    //event.data.fd = connection_socket_descriptor;
-    //epoll_ctl(epfd, EPOLL_CTL_ADD, connection_socket_descriptor, &event);
-
-    memset(&server_address, 0, sizeof(struct sockaddr));
-    server_address.sin_family = AF_INET;
-    memcpy(&server_address.sin_addr.s_addr, server_host_entity->h_addr, server_host_entity->h_length);
-    server_address.sin_port = htons(atoi(argv[2]));
-
-    connect_result = connect(connection_socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
-    while (connect_result < 0)
-    {
-        fprintf(stderr, "%s: Blad przy probie podlczenia z serwerem (%s:%i).\n", argv[0], argv[1], atoi(argv[2]));
-        sleep(5);
-        connect_result = connect(connection_socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
-        //exit(1);
-    }
-
-    char* buf;
-    //printf("Chce odebrac wiadomosc zwrotna\n");
-    //recv(connection_socket_descriptor, buf, sizeof(buf), MSG_WAITALL);
-    //printf("Odebralem\n");
-    //sleep(4);
-
-    //printf("Halo\n");
-    char* znaki[40];
-    char want[4] = "want";
-    char answer[20] = "";
-    handleConnection(connection_socket_descriptor);
     while(1){
-        printf("Wpisz cos\n");
-
-        scanf("%40s",&znaki);
-
-        while (strlen(answer) == 0) {
-            printf("WCHODZE \n");
-            send(connection_socket_descriptor, want, sizeof(want), MSG_CONFIRM);
-            sleep(2);
-            recv(connection_socket_descriptor, answer, sizeof(answer), MSG_DONTWAIT);
-            printf("%s\n", answer);
-            sleep(2);
+        if((epoll_des = epoll_wait(epoll_fd, events,MAX_EVENTS, -1)) == -1) {
+            printf("epoll wait\n");
         }
-        memset(answer, 0, 6);
+        printf("Wskaznik NFDS: %d\n", epoll_des);
+        for (i = 0; i < epoll_des; ++i) {
+            if (events[i].data.fd == server_socket_descriptor) {
+                struct sockaddr_in client_sock;
+                socklen_t slen = sizeof(client_sock);
+                connection_socket_descriptor = accept(server_socket_descriptor, (struct sockaddr*)&client_sock, &slen);
+                //fcntl(connection_socket_descriptor, F_SETFL, O_NONBLOCK); <--- ta komenda sprawia ze nie czeka na read
+                if (connection_socket_descriptor < 0) {
+                    fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda dla połączenia.\n", argv[0]);
+                    exit(1);
+                }
 
-        printf("Wysylam komende uzytkownika: %s\n", znaki);
-        //send(connection_socket_descriptor, znaki, sizeof(znaki), MSG_CONFIRM);
+                event.events = EPOLLIN | EPOLLRDHUP;
+                event.data.fd = connection_socket_descriptor;
 
+                if(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, connection_socket_descriptor, &event) == -1) {
+                    fprintf(stderr, "Blad epoll_ctl ADD dla klawiatury\n");
+                    close(epoll_fd);
+                    return 1;
+                }
+
+                char* buf[100] = {};
+                int read_user = read(connection_socket_descriptor, buf, sizeof(buf));
+
+                //char* temp;
+                *temp = malloc(strlen(path)+1);
+                strcpy(temp, path);
+
+                connectUser(buf, temp);
+                map_clients[counter].path = temp;
+
+                printBreak();
+                printf("Operacja ACCEPT dla deskryptora: %d zakonczona sukcesem\n", connection_socket_descriptor);
+                printBreak();
+                printf("\n");
+
+                map_clients[counter].key = connection_socket_descriptor;
+                map_clients[counter].var = " ";
+
+                //handleConnection(connection_socket_descriptor, t_data_ptr, array, path, epoll_fd, &event, events);
+
+            }
+            else{
+                printf("Tu beda komendy\n");
+                printf("%d\n", events[i].data.fd);
+                KeyFunction(events[i].data.fd, connection_socket_descriptor, temp);
+            }
+        }
     }
 
-    //handleConnection(connection_socket_descriptor);
+    // zamknięcie gniazda
+    close(connection_socket_descriptor);
 
+    // usuń mutexy
+        //for (int i = 0; i < 3; i++)
+            //pthread_mutex_destroy(locks[i]);
+    //pthread_mutex_destroy(&(t_data_ptr->thread_data_mutex));
 
-    /*while(1) {
-        recv(connection_socket_descriptor, buf, 12, 0);
-        printf("Odebralem wiadomosc\n");
-    }*/
-    sleep(10000);
-    close(connect_result);
-    return 0;
+    // uwalnianie pamięci
+    free(t_data_ptr->client_socketfd);
+    free(t_data_ptr);
+
+    return(0);
 }
-
